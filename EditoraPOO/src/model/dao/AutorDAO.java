08f680f2b4522d6bb90a06b73363a68ebbb0dea9
RaquelDAlmeida <80779425+RaquelDAlmeida@users.noreply.github.com>
2021-09-30 22:11:32 -0300

    Refatoração Classe AutorDAO

--- EditoraPOO/src/model/dao/AutorDAO.java
+++ EditoraPOO/src/model/dao/AutorDAO.java
@@ -10,66 +10,63 @@ import java.util.List;
 import VO.AutorVO;
 import VO.UsuarioVO;
 
-public class AutorDAO extends BaseDAO {
-	
+public class AutorDAO extends BaseDAO<AutorVO> {
+	@Override
 	public void inserir(AutorVO vo) throws SQLException {
+		try {
+			super.inserir(vo);
 			conn = getConnection();
 			String sql = "insert into Autor (cpf, endereco, idUsuario) values (?,?,?)";
 			PreparedStatement ptst;
-			try {
-				ptst = conn.prepareStatement(sql);
-				ptst.setString(1, vo.getCpf());
-				ptst.setString(2, vo.getEndereco());
-				ptst.setLong(3, vo.getUsuario().getId());
-				ptst.execute();
-			} catch (SQLException e) {
-				e.printStackTrace();
+			ptst = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			ptst.setString(1, vo.getCpf());
+			ptst.setString(2, vo.getEndereco());
+			ptst.setLong(3, vo.getUsuario().getId());
+			int affectedRows = ptst.executeUpdate();
+			if(affectedRows == 0) {
+				throws new SQLException('A inserção falhou!');
 			}
-			
-		}
-
-		public void remover(AutorVO vo) throws SQLException {
-			conn = getConnection();
-			String sql = "delete from Autor where id = ?";
-			PreparedStatement ptst;
-			try {
-				ptst = conn.prepareStatement(sql);
-				ptst.setLong(1, vo.getId());
-				ptst.executeUpdate();
-			} catch (SQLException e) {
-				e.printStackTrace();
+			ResultSet generatedKeys = ptst.getGeneratedKeys();
+			if(generatedKeys.next()){
+				vo.setIDFunc(generatedKeys.getLong(1));
+			} else {
+				throws new SQLException('A inserção falhou!');
 			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}	
+	}
+	@Override
+	public void remover(VO vo) throws SQLException {
+		conn = getConnection();
+		String sql = "delete from Autor where id = ?";
+		PreparedStatement ptst;
+		try {
+			ptst = conn.prepareStatement(sql);
+			ptst.setLong(1, vo.getId());
+			ptst.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
-
-		public List<AutorVO> listar() throws SQLException {
-			conn = getConnection();
-			String sql = "select * from Autor";
-			Statement st;
-			ResultSet rs;
-			List<AutorVO> autores = new ArrayList<AutorVO>();
+	}
+	@Override
+	public ResultSet listar(VO vo) throws SQLException {
+		conn = getConnection();
+		String sql = "select * from Autor";
+		PreparedStatement st;
+		ResultSet rs = null;
 			
-			try {
-				st = conn.createStatement();
-				rs = st.executeQuery(sql);
-				while(rs.next()){
-					AutorVO vo = new AutorVO(null, null, null);
-					vo.setCpf(rs.getString("cpf"));
-					vo.setEndereco(rs.getString("endereco"));
-					vo.setId(rs.getLong("id"));
-					UsuarioDAO user = new UsuarioDAO();
-					UsuarioVO uservo = user.getUsuario(rs.getLong("idUsuario"));
-					vo.setUsuario(uservo);
-					autores.add(vo);
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			return autores;
-			
+		try {
+			st = conn.createStatement(sql);
+			rs = st.executeQuery(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
+		return rs;	
+	}
 
-
-		public void editar(AutorVO vo) throws SQLException {
+	@Override
+	public void editar(VO vo) throws SQLException {
 			conn = getConnection();
 			String sql = "update from Autor set cpf = ?, endereco = ?, idUsuario = ? where id = ?";
 			PreparedStatement ptst;
